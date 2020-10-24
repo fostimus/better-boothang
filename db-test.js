a43c0bd6f0a71ec8d1d875023f07484c24d9febc
Derek Foster <derekfoster94@gmail.com>
2020-10-23 22:19:04 -0700

    createBoothang method tested

--- db-test.js
+++ db-test.js
@@ -11,12 +11,29 @@ db.user
     db.boothang
       .findOrCreate({
         where: {
-          name: "Gayle"
+          name: "Linda"
         }
       })
       .then(([returnedBooThang, created]) => {
         returnedUser.addBoothang(returnedBooThang);
         console.log("---------");
         console.log(`${returnedUser.firstName} added ${returnedBooThang.name}`);
+      });
+  });
+
+db.user
+  .findOrCreate({
+    where: {
+      firstName: "Linda",
+      lastName: "Belcher"
+    }
+  })
+  .then(([returnedUser, created]) => {
+    returnedUser
+      .createBoothang({
+        name: "Bob"
+      })
+      .then(boothang => {
+        console.log(`${returnedUser.firstName} created ${boothang.name}`);
       });
   });
