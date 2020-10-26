c236865688d9be22cfa306dded57cd29cddd1edb
Derek Foster <derekfoster94@gmail.com>
2020-10-26 11:38:34 -0700

    put client call into helper method; set up messages page with sample json messages

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const db = require("../models");
 const router = express.Router();
+const fs = require("fs");
 
 const client = require("twilio")(
   process.env.ACCOUNT_SID,
@@ -13,20 +14,33 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", function(req, res) {
-  client.messages
-    .create({
-      body: "TESTING FROM THE ATOMIC CODE GHOSTS! boo",
-      from: process.env.TWILIO_NUM,
-      to: req.body.phoneNumber
-    })
-    .then(message => {
-      console.log(message.sid);
-    });
+  sendText(req.body.phoneNumber);
   res.redirect("/profile");
 });
 
 router.get("/messages", (req, res) => {
-  res.render("user/messages.ejs");
+  const messages = fs.readFileSync("./sample-messages.json");
+  const messageData = JSON.parse(messages);
+
+  res.render("user/messages", { messages: messageData });
 });
+
+router.post("/messages", (req, res) => {
+  sendText(req.body.phoneNumber, req.body.message);
+  res.redirect("/profile");
+});
+
+const sendText = (phoneNumber, message) => {
+  const messageToSend = message || "TESTING FROM THE ATOMIC CODE GHOSTS! boo";
+  client.messages
+    .create({
+      body: messageToSend,
+      from: process.env.TWILIO_NUM,
+      to: phoneNumber
+    })
+    .then(message => {
+      console.log(message.sid);
+    });
+};
 
 module.exports = router;
