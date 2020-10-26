8393e920e7fa1b2f596c73cb69ef9fc95ee2bf6e
Derek Foster <derekfoster94@gmail.com>
2020-10-26 11:01:55 -0700

    working input field for phone number and sending text message

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,32 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+const db = require("../models");
 const router = express.Router();
 
-router.get('/booInfo.ejs', (req, res) => {
-    res.render('auth/booInfo.ejs')
-})
+const client = require("twilio")(
+  process.env.ACCOUNT_SID,
+  process.env.AUTH_TOKEN
+);
 
-router.get('/messages.ejs', (req, res) => {
-    res.render('auth/messages.ejs')
-})
+router.get("/", (req, res) => {
+  res.render("user/booInfo.ejs");
+});
+
+router.post("/", function(req, res) {
+  client.messages
+    .create({
+      body: "TESTING FROM THE ATOMIC CODE GHOSTS! boo",
+      from: process.env.TWILIO_NUM,
+      to: req.body.phoneNumber
+    })
+    .then(message => {
+      console.log(message.sid);
+    });
+  res.send("hey");
+});
+
+router.get("/messages.ejs", (req, res) => {
+  res.render("auth/messages.ejs");
+});
 
 module.exports = router;
