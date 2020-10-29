e3914b14400cb3f4182639412f17e9b78f7c4ce8
Derek Foster <derekfoster94@gmail.com>
2020-10-29 12:38:50 -0700

    set up form and client side js for sending to one or all boothangs

--- public/client.js
+++ public/client.js
@@ -6,3 +6,21 @@ messageLinks.forEach(messageLink => {
     document.querySelector("#message-to-boo").value = messageLink.textContent;
   });
 });
+
+// click handler to change value of hidden input AND drop down button text to selcted value
+const chosenBoothangButtons = document.querySelectorAll(".choose-boothang");
+chosenBoothangButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    document.querySelector("#boothangDropdownButton").textContent =
+      button.textContent;
+    document.querySelector("#sendToChosenBoothang").textContent =
+      "Send to " + button.textContent;
+    document.querySelector("#chosenBoothang").value = button.textContent;
+  });
+});
+
+//ensure hidden input value for chosenBoothang is ""
+const sendToAll = document.querySelector("sendToAllBoothangs");
+sendToAll.addEventListener("click", () => {
+  document.querySelector("#chosenBoothang").value = "";
+});
