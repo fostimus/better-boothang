2fae892710e9c16fa606b8eb96a06dc797ab7f48
Derek Foster <derekfoster94@gmail.com>
2020-10-29 15:45:04 -0700

    FIXED THE BUGgit statusgit status wasn't sending the chosen boothang properly, needs to be json since using fetch and not form data

--- public/client.js
+++ public/client.js
@@ -14,27 +14,26 @@ if (clearInput) {
   });
 }
 
+// TODO: this is a terrible way to do this. should use the value provided in user.js server side, not hard code the name
+const sendModalTitle = "send-boothang-modal";
+
 // click handler to ensure send modal is displaying the proper initial state: a dropdown
 const initiateSendButton = document.querySelector("#initiate-send");
 if (initiateSendButton) {
   initiateSendButton.addEventListener("click", () => {
     const chooseBoo = document.querySelector(".choose-yo-boo");
-    console.log(chooseBoo);
     if (chooseBoo.classList.contains("hidden")) {
       chooseBoo.classList.toggle("hidden");
     }
 
     const sentToBoo = document.querySelector(".sent");
-    console.log(sentToBoo);
     if (!sentToBoo.classList.contains("hidden")) {
       sentToBoo.classList.toggle("hidden");
     }
 
-    // TODO: this is a terrible way to do this. should use the value provided in user.js server side, not hard code the name
     const modalTitle = document.querySelector(
-      "#send-boothang-modal .modal-title"
+      "#" + sendModalTitle + " .modal-title"
     );
-    console.log(modalTitle);
     modalTitle.textContent = "Choose which BooThang";
   });
 }
@@ -50,12 +49,6 @@ chosenBoothangButtons.forEach(button => {
     document.querySelector("#chosenBoothang").value = button.getAttribute(
       "boothangId"
     );
-
-    console.log("button boothang id: " + button.getAttribute("boothangId"));
-    console.log(
-      "chosenBoothang boothang id: " +
-        document.querySelector("#chosenBoothang").value
-    );
   });
 });
 
@@ -70,13 +63,7 @@ if (sendToChosen) {
 const sendToAll = document.querySelector("#sendToAllBoothangs");
 if (sendToAll) {
   sendToAll.addEventListener("click", e => {
-    console.log(
-      "hidden input value: " + document.querySelector("#chosenBoothang").value
-    );
     document.querySelector("#chosenBoothang").value = "";
-    console.log(
-      "hidden input value: " + document.querySelector("#chosenBoothang").value
-    );
     sendMessageJson(e);
   });
 }
@@ -89,16 +76,15 @@ function sendMessageJson(e) {
   e.preventDefault();
 
   const json = {
+    chosenBoothangId: document.querySelector("#chosenBoothang").value,
     message: document.querySelector("#message-to-boo").value
   };
   postData("/user/messages", json).then(data => {
     console.log(data);
     if (data === 200) {
-      // TODO: this is a terrible way to do this. should use the value provided in user.js server side, not hard code the name
       const modalTitle = document.querySelector(
-        "#send-boothang-modal .modal-title"
+        "#" + sendModalTitle + " .modal-title"
       );
-      console.log(modalTitle);
       modalTitle.textContent = "Message sent!";
 
       document.querySelector(".choose-yo-boo").classList.toggle("hidden");
