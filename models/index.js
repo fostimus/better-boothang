5a2404e87810b2c462b80c7ce8708d63f22e64b4
Derek Foster <derekfoster94@gmail.com>
2021-01-05 10:04:51 -0800

    added ssl to url in index

--- models/index.js
+++ models/index.js
@@ -1,27 +1,38 @@
-'use strict';
+"use strict";
 
-const fs        = require('fs');
-const path      = require('path');
-const Sequelize = require('sequelize');
-const basename  = path.basename(module.filename);
-const env       = process.env.NODE_ENV || 'development';
-const config    = require(__dirname + '/../config/config.json')[env];
-const db        = {};
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
+const basename = path.basename(module.filename);
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../config/config.json")[env];
+const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable]);
+  const url = process.env[config.use_env_variable] + "?ssl=true";
+  console.log(url);
+  sequelize = new Sequelize(url);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
 }
 
-fs
-  .readdirSync(__dirname)
+fs.readdirSync(__dirname)
   .filter(function(file) {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
   })
   .forEach(function(file) {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
     db[model.name] = model;
   });
 
@@ -35,3 +46,5 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
+
+// postgres://ptjxhrnojdnjwc:856a1f77a3ea175ea9ad8ab9add8bf8d1378eb09b7d632f3afd47a59e4b8885f@ec2-52-44-46-66.compute-1.amazonaws.com:5432/dc9ooj4ogtldem
