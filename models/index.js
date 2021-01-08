b4185b9438d28c21f066089a819c81ad177327ab
Derek Foster <derekfoster94@gmail.com>
2021-01-08 08:02:34 -0800

    self signed ok

--- models/index.js
+++ models/index.js
@@ -10,7 +10,15 @@ const db = {};
 
 let sequelize;
 if (process.env.DATABASE_URL) {
-  sequelize = new Sequelize(process.env.DATABASE_URL);
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
+  });
 } else if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], {
     dialect: "postgres",
