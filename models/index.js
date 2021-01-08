bd11499f384eabc3e328bcfedb3d40fb0678a79a
Derek Foster <derekfoster94@gmail.com>
2021-01-08 07:15:02 -0800

    changed index to use db url directly if it exists

--- models/index.js
+++ models/index.js
@@ -9,7 +9,15 @@ const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    logging: false,
+    dialectOptions: {
+      ssl: true
+    }
+  });
+} else if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], {
     dialect: "postgres",
     logging: false,
